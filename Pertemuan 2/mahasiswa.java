b4010dfb7755ee957ca04034baeb34069d92b9fe
Arum Agam Nugraha <arumagam45@gmail.com>
2017-02-28 15:02:11 +0700

    Add files via upload

---
+++ Pertemuan 2/mahasiswa.java
@@ -0,0 +1,7 @@
+class mahasiswa {
+  public static void main (String[] args){
+   System.out.println("Nama : Arum Agam Nugraha");
+   System.out.println("NIM : 10115168");
+   System.out.println("Alamat : Jl.sekeloa Gg kubang sari 7 n 22");
+  }
+}
