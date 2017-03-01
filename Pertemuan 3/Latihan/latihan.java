5505ff74406659033e87fed1b8acf1815dd1f064
Arum Agam Nugraha <arumagam45@gmail.com>
2017-03-01 17:12:15 +0700

    Add files via upload

---
+++ Pertemuan 3/Latihan/latihan.java
@@ -0,0 +1,6 @@
+class latihan {
+  public static void main (String[] args){
+   System.out.print("Belajar java yuks!");
+   System.out.println("Belajar java yuks 1");
+  }
+}
---
+++ Pertemuan 3/latihan2/latihan2.java
@@ -0,0 +1,7 @@
+class latihan2 {
+  public static void main (String[] args){
+   System.out.print("Belajar java yuks!");
+   System.out.println("Belajar java yuks 1");
+   System.out.print("Belajar \njava \nyuks 1");
+  }
+}
