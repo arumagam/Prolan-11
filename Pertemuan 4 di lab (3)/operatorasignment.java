528a3e6b7cb4d18eada923e6d0e81fe8a2f7d7ca
Arum Agam Nugraha <arumagam45@gmail.com>
2017-03-15 03:53:39 +0000

    Add files via upload

---
+++ Pertemuan 4 di lab (3)/operatorasignment.java
@@ -0,0 +1,13 @@
+public class operatorasignment {
+	public static void main(String[]args) {
+		int x = 10;
+		System.out.println(x);
+		System.out.println(x +=10);
+		System.out.println(x -=10);
+		System.out.println(x*=10);
+		System.out.println(x/=10);
+		System.out.println(x%=10);
+		x=10;
+		System.out.println((x==1)?11:10);
+	}
+}
