4a4bb1fd9a601bee7d895fc1da456ba97f5fdb00
Arum Agam Nugraha <arumagam45@gmail.com>
2017-05-17 23:06:31 +0700

    Add files via upload

---
+++ Arum Agam Nugraha-10115168-Encaplntegration/motor.java
@@ -0,0 +1,36 @@
+public class motor {
+	private String nama;
+	private int banyak,gear,rantai;
+	private int harga;
+	
+	public String getnama () {
+		return nama;
+	}
+	public void setnama (String nama) {
+		this.nama = nama;
+	}
+	public int getbanyak () {
+		return banyak;
+	}
+	public void setbanyak (int banyak) {
+		this.banyak = banyak;
+	}
+	public int getgear () {
+		return gear;
+	}
+	public void setgear (int gear) {
+		this.gear = gear;
+	}
+	public int getrantai () {
+		return rantai;
+	}
+	public void setrantai (int rantai) {
+		this.rantai = rantai;
+	}
+	public int getharga () {
+		return harga;
+	}
+	public void setharga (int harga) {
+		this.harga = harga;
+	}
+}
